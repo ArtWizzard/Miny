2658901218253acd1233438f5669c222ce257a3c
ArtWizzard <samotkecemen@seznam.cz>
2021-05-20 22:34:32 +0200

    Inicializace pole a úvodní verze objektu pro obsluhu konzolového vstupu.

--- miny.py
+++ miny.py
@@ -5,12 +5,38 @@ Python 3.8
 @author: samot
 """
 
-import random, pygame
+# import random, pygame
+
+class POLE():
+    def __init__(self):
+        self.cell_number = 10
+        self.pole = []
+        self.pole_init()
+
+    
+    def pole_init(self):
+        temp = []
+        for i in range(self.cell_number):
+            temp.append(0)
+        for i in range(self.cell_number):
+            self.pole.append(temp)
+    
+    def draw_pole(self):
+        print(self.pole)
+
+class HPI():
+    def console_input_coordinates(self, souradnice):
+        try:
+            return_souradnice = int(input("Zadej "+str(souradnice)+"-ovou souřadnici: "))
+            return return_souradnice
+        except:
+            print("Chybný formát souřadnic, souřadnice je celé číslo od 0 po "+str(pole.cell_number-1))
+
+
+pole = POLE()
+hpi = HPI()
 
 while True:
-    print(input("ahoj: "))
-    if True:
-        print(random.randint(1,2))
-    print(5+1)
-    for i in range(5):
-        print("ahoj\n")
+    pole.draw_pole()
+    
+    hpi.console_input_coordinates("x")
